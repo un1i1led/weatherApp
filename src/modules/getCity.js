774879c38d48ef8d342c10bce1a80a9a338b58eb
gilberto <gsqwow@gmail.com>
2022-11-05 00:04:15 -0700

    Change functions to async functions

--- src/modules/getCity.js
+++ src/modules/getCity.js
@@ -1,28 +1,21 @@
-const getWeather = (lat, lon) => {
-  const call = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d8d4bc4545de4700adce2ef463767cc7`;
-  fetch(call, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-    });
-};
+async function getWeather(lat, lon) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d8d4bc4545de4700adce2ef463767cc7`, { mode: 'cors' });
+  const data = await response.json();
+  return data;
+}
 
 const checkCityValid = (response, location) => {
   if (location.length == response[0].name.length) {
-    getWeather(response[0].lat, response[0].lon);
-  } else {
-    console.log('Not a valid city');
+    return getWeather(response[0].lat, response[0].lon);
   }
+  return 'Not a valid city';
 };
 
-const makeCall = (location) => {
-  const call = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=d8d4bc4545de4700adce2ef463767cc7`;
-
-  fetch(call, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((response) => {
-      checkCityValid(response, location);
-    });
-};
+async function makeCall(location) {
+  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=d8d4bc4545de4700adce2ef463767cc7`);
+  const data = await response.json();
+  const stuff = await checkCityValid(data, location);
+  return stuff;
+}
 
 export default makeCall;
