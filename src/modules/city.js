2cef5196819d938e5da5f7331427799258d6df83
gilberto <gsqwow@gmail.com>
2022-11-05 20:56:42 -0700

    Add toCelsius, toFahr and wind to city obj

--- src/modules/city.js
+++ src/modules/city.js
@@ -1,11 +1,20 @@
-const city = (name, temp, feels, description, humidity, min, max) => ({
-  name,
-  temp,
-  feels,
-  description,
-  humidity,
-  min,
-  max,
-});
+const city = (name, temp, feels, description, humidity, min, max, wind, country) => {
+  const toCelsius = (value) => Math.round(value - 273.15);
+  const toFahr = (value) => Math.round(((value - 273.15) * (9 / 5)) + 32);
+
+  return {
+    name,
+    temp,
+    feels,
+    description,
+    humidity,
+    min,
+    max,
+    wind,
+    country,
+    toCelsius,
+    toFahr,
+  };
+};
 
 export default city;
