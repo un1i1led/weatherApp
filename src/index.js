efdabc2db2db4f34282faa4044d522217cec2e94
gilberto <gsqwow@gmail.com>
2022-11-05 20:56:03 -0700

    Add async location function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import './style.css';
-import makeCall from './modules/getPlace';
+import makeCall from './modules/getCity';
+import initUI from './modules/ui';
 
 const btn = document.querySelector('#btn-search');
 const input = document.querySelector('#search');
 
 const getLocation = () => {
-  const location = document.querySelector('#search').value;
-  return location;
+  const locationValue = document.querySelector('#search').value;
+  return locationValue;
 };
+
+async function location() {
+  const call = await makeCall(getLocation());
+  initUI(call);
+}
 
 btn.addEventListener('click', () => {
   if (input.value === '') {
     console.log('nothin to show');
   } else {
-    makeCall(getLocation());
+    location();
   }
 });
