8d53d44b5f76e3d953e79773bc00077da6d0779f
gilberto <gsqwow@gmail.com>
2022-11-08 18:39:36 -0700

    Change api fetch to get data by city name instead of lat and lon

--- src/modules/getCity.js
+++ src/modules/getCity.js
@@ -1,11 +1,6 @@
-async function getWeather(lat, lon) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d8d4bc4545de4700adce2ef463767cc7`, { mode: 'cors' });
-  const data = await response.json();
-  return data;
-}
-
 async function loadImage(data) {
   const icon = new Image();
+  console.log(data.weather[0].icon);
   icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   console.log(data.weather[0].icon);
   await icon.decode();
@@ -13,17 +8,23 @@ async function loadImage(data) {
 }
 
 const checkCityValid = (response, location) => {
-  if (location.length == response[0].name.length) {
-    return getWeather(response[0].lat, response[0].lon);
+  const newName = response.name.toLowerCase();
+  const newLocation = location.replace('+', ' ').toLowerCase();
+  console.log(newLocation);
+  if (newName == newLocation) {
+    return true;
   }
-  return 'Not a valid city';
+  return false;
 };
 
 async function makeCall(location) {
-  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=d8d4bc4545de4700adce2ef463767cc7`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=d8d4bc4545de4700adce2ef463767cc7`);
   const data = await response.json();
-  const stuff = await checkCityValid(data, location);
-  return stuff;
+  const stuff = checkCityValid(data, location);
+  if (stuff) {
+    return data;
+  }
+  return 'Not a valid city';
 }
 
 export {
