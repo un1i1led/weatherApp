c2383db89953b0ba4ca0103c8c21515e7217e8c3
gilberto <gsqwow@gmail.com>
2022-11-08 19:29:14 -0700

    Change conversion func to convert from Celsius

--- src/modules/city.js
+++ src/modules/city.js
@@ -1,6 +1,6 @@
 const city = (name, temp, feels, description, humidity, min, max, wind, country) => {
-  const toCelsius = (value) => Math.round(value - 273.15);
-  const toFahr = (value) => Math.round(((value - 273.15) * (9 / 5)) + 32);
+  const toCelsius = (value) => Math.round(value);
+  const toFahr = (value) => Math.round((value * (9 / 5)) + 32);
 
   return {
     name,
