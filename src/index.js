4e40a86af062ceedc306d9689ed8925182abfe09
gilberto <gsqwow@gmail.com>
2022-11-12 21:56:58 -0700

    Add eventlistener to enter to start process

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { makeCall, loadImage, loadBackIcon } from './modules/getCity';
 import initUI from './modules/ui';
 
 const btn = document.querySelector('.search-btn');
-const input = document.querySelector('#search');
+const input = document.querySelector('#search-input');
 
 const checkSpaces = (string) => string.replace(' ', '+');
 
 const getLocation = () => {
-  let locationValue = document.querySelector('#search').value;
+  let locationValue = document.querySelector('#search-input').value;
   locationValue = (checkSpaces(locationValue));
   return locationValue;
 };
@@ -21,10 +21,17 @@ async function location() {
   initUI(call, loadIcon, backIcon);
 }
 
+document.getElementById('form').addEventListener('keypress', (e) => {
+  if (e.key == 'Enter') {
+    e.preventDefault();
+    if (input.value != '') {
+      location();
+    }
+  }
+});
+
 btn.addEventListener('click', () => {
-  if (input.value === '') {
-    console.log('nothin to show');
-  } else {
+  if (input.value != '') {
     location();
   }
 });
